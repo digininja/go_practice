ce3542b7470d29e6283afa719c4fef1cd537ef55
Robin Wood <robin@digi.ninja>
2018-06-05 22:46:01 +0100

    Playing with regex

---
+++ regexp/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+import "regexp"
+
+func main() {
+	fmt.Println("vim-go")
+	var year_end = regexp.MustCompile(`((19[0-9][0-9])|(20[012][0-9]))$`)
+
+	fmt.Printf("%q\n", year_end.FindString("abc1999"))
+	fmt.Printf("%q\n", year_end.FindString("1999abc"))
+	fmt.Printf("%q\n", year_end.FindString("1999"))
+	fmt.Printf("%q\n", year_end.FindString("asdsdfasdf"))
+	fmt.Printf("%q\n", year_end.FindString("2020x"))
+	fmt.Printf("%q\n", year_end.FindString("x2020"))
+
+}
