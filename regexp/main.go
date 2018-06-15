b9e8b8d34c1da61a9301e221f130c776cce744d8
Robin <robin@digi.ninja>
2018-06-15 11:26:56 +0100

    more strings

--- regexp/main.go
+++ regexp/main.go
@@ -17,15 +17,95 @@ func main() {
 	fmt.Println("first cap last number")
 	first_cap_last_num_re := regexp.MustCompile("^[A-Z].*[0-9]$")
 
-	fmt.Printf("%q\n", first_cap_last_num_re.MatchString("Axxx9"))
+	fmt.Printf("%t\n", first_cap_last_num_re.MatchString("Axxx9"))
 
 	fmt.Println("first cap last symbol")
 
 	first_cap_last_symbol_re := regexp.MustCompilePOSIX("^[A-Z].*[[:punct:]]$")
 
-	fmt.Printf("%q\n", first_cap_last_symbol_re.MatchString("Axxx9"))
-	fmt.Printf("%q\n", first_cap_last_symbol_re.MatchString("Axxx$"))
-	fmt.Printf("%q\n", first_cap_last_symbol_re.MatchString("Axxx%"))
-	fmt.Printf("%q\n", first_cap_last_symbol_re.MatchString("Axxx"))
+	fmt.Printf("%t\n", first_cap_last_symbol_re.MatchString("Axxx9"))
+	fmt.Printf("%t\n", first_cap_last_symbol_re.MatchString("Axxx$"))
+	fmt.Printf("%t\n", first_cap_last_symbol_re.MatchString("Axxx%"))
+	fmt.Printf("%t\n", first_cap_last_symbol_re.MatchString("Axxx"))
 
+	// https://golang.org/pkg/regexp/syntax/
+	const Symbols_Regexp = "=!\"£$%^&*()\\-_\\[\\]#~'@;:<>,./?{}"
+
+	fmt.Println("\n\npunctuation\n\n")
+	re := regexp.MustCompile(fmt.Sprintf("^[%s]+$", Symbols_Regexp))
+	fmt.Printf("%t\n", re.MatchString("Axxx"))
+	fmt.Printf("%t\n", re.MatchString("..."))
+	fmt.Printf("%t\n", re.MatchString("!!"))
+	fmt.Printf("%t\n", re.MatchString("@:!"))
+	fmt.Printf("%t\n", re.MatchString("\"aaa\""))
+	fmt.Printf("%t\n", re.MatchString("\"\""))
+	fmt.Printf("%t\n", re.MatchString("£$%"))
+
+	fmt.Println("\n\ncombine\n\n")
+	re_az := regexp.MustCompile("[a-z]+")
+	re_AZ := regexp.MustCompile("[A-Z]+")
+	re_09 := regexp.MustCompile("[0-9]+")
+	re_symb := regexp.MustCompile(fmt.Sprintf("[%s]+", Symbols_Regexp))
+	re_exclude := regexp.MustCompile(fmt.Sprintf("^[A-Za-z0-9%s]+$", Symbols_Regexp))
+	s := "abcA$3"
+	if re_az.MatchString(s) && re_AZ.MatchString(s) && re_09.MatchString(s) && re_symb.MatchString(s) && re_exclude.MatchString(s) {
+		fmt.Printf("Match\n")
+	} else {
+		fmt.Printf("No Match\n")
+	}
+	s = "abc$3"
+	if re_az.MatchString(s) && re_AZ.MatchString(s) && re_09.MatchString(s) && re_symb.MatchString(s) && re_exclude.MatchString(s) {
+		fmt.Printf("Match\n")
+	} else {
+		fmt.Printf("No Match\n")
+	}
+	s = "ABC$3"
+	if re_az.MatchString(s) && re_AZ.MatchString(s) && re_09.MatchString(s) && re_symb.MatchString(s) && re_exclude.MatchString(s) {
+		fmt.Printf("Match\n")
+	} else {
+		fmt.Printf("No Match\n")
+	}
+	s = "AB$3"
+	if re_az.MatchString(s) && re_AZ.MatchString(s) && re_09.MatchString(s) && re_symb.MatchString(s) && re_exclude.MatchString(s) {
+		fmt.Printf("Match\n")
+	} else {
+		fmt.Printf("No Match\n")
+	}
+	s = "Aaaaaaaaaaaaaaaaaaaaaaaaaaaa.B$3"
+	if re_az.MatchString(s) && re_AZ.MatchString(s) && re_09.MatchString(s) && re_symb.MatchString(s) && re_exclude.MatchString(s) {
+		fmt.Printf("Match\n")
+	} else {
+		fmt.Printf("No Match\n")
+	}
+	s = "aB4$"
+	if re_az.MatchString(s) && re_AZ.MatchString(s) && re_09.MatchString(s) && re_symb.MatchString(s) && re_exclude.MatchString(s) {
+		fmt.Printf("Match\n")
+	} else {
+		fmt.Printf("No Match\n")
+	}
+
+	fmt.Println("\n\nnumbers on end\n\n")
+
+	s = "abc3"
+	re = regexp.MustCompile(".*([0-9]{1})$")
+	hit := re.FindString(s)
+	if hit != "" {
+		fmt.Printf("hit: %s\n", hit)
+	}
+
+	re = regexp.MustCompile(".*([0-9]{1})$")
+	fmt.Println(re.FindStringIndex(s))
+	loc := (re.FindStringIndex(s))
+
+	fmt.Println(s[loc[0]:loc[1]])
+
+	fmt.Println(re.FindStringSubmatch(s))
+	s = "xxx"
+	fmt.Println(re.FindStringSubmatch(s))
+
+	s = "xxx44"
+	fmt.Println(re.FindStringSubmatch(s))
+	re = regexp.MustCompile(".*([0-9]{2})$")
+	s = "xxx44"
+	fmt.Println(re.FindStringSubmatch(s))
 }
