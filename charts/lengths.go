07c448ac50f9ac2c522a1d2849c9a782a2c8811c
Robin Wood <robin@digi.ninja>
2018-10-13 21:45:45 +0100

    broken into function

--- charts/lengths.go
+++ charts/lengths.go
@@ -8,57 +8,70 @@ import (
 )
 
 import "math/rand"
-import "fmt"
 import "time"
 import "strconv"
 
-func main() {
-	var lengths = [10]int{}
+// Used in the plotter colours
+const RED = 0
+const GREEN = 1
+const BLUE = 2
+const WIDTH = 5 * vg.Inch
+const HEIGHT = 3 * vg.Inch
 
-	// not crypto secure generateion of seed
-	seed := rand.NewSource(time.Now().UnixNano())
-	random := rand.New(seed)
-
-	for i := 0; i < 10; i++ {
-		lengths[i] = random.Intn(50)
-	}
-
+func GenBarChart(filename string, title string, ylabel string, xlabels []string, data []float64) {
 	groupAY := plotter.Values{}
-	for length, count := range lengths {
-		fmt.Println("Length:", length, "Value:", count)
-		groupAY = append(groupAY, float64(count))
+
+	for _, val := range data {
+		groupAY = append(groupAY, float64(val))
 	}
 
 	p, err := plot.New()
 	if err != nil {
 		panic(err)
 	}
-	p.Title.Text = "Password Length"
-	p.Y.Label.Text = "Number of characters"
+	p.Title.Text = title
+	p.Y.Label.Text = ylabel
 
-	w := vg.Points(10)
+	// Default font size
+	w := vg.Points(50)
 
 	barsA, err := plotter.NewBarChart(groupAY, w)
 	if err != nil {
 		panic(err)
 	}
-	barsA.LineStyle.Width = vg.Length(0)
-	barsA.Color = plotutil.Color(0)
-	barsA.Offset = -w
+	// barsA.LineStyle.Width = vg.Length(0)
+	// barsA.Offset = -w
+	barsA.Color = plotutil.Color(BLUE)
+	barsA.Width = 20
 
-	keys := make([]string, len(lengths))
+	p.Add(barsA)
+	// p.Legend.Add("Group A", barsA)
+	p.NominalX(xlabels...)
+
+	if err := p.Save(WIDTH, HEIGHT, filename); err != nil {
+		panic(err)
+	}
+
+}
+
+func main() {
+	var lengths = make([]float64, 10)
+
+	// not crypto secure generateion of seed
+	seed := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(seed)
+
+	for i := 0; i < 10; i++ {
+		lengths[i] = float64(random.Intn(50))
+	}
+
+	var xlabels = make([]string, len(lengths))
 
 	i := 0
 	for k := range lengths {
-		keys[i] = strconv.Itoa(k)
+		xlabels[i] = strconv.Itoa(k)
 		i++
 	}
 
-	p.Add(barsA)
-	//p.Legend.Add("Group A", barsA)
-	p.NominalX(keys...)
-
-	if err := p.Save(5*vg.Inch, 3*vg.Inch, "lengths.png"); err != nil {
-		panic(err)
-	}
+	GenBarChart("lengths.png", "Password Length", "Number of characters", xlabels, lengths)
 }
