c8a33a8cb04855314f326341c126bce976f59906
Robin Wood <robin@digi.ninja>
2019-04-07 12:35:22 +0100

    working with validation

--- revel/myapp/app/controllers/app.go
+++ revel/myapp/app/controllers/app.go
@@ -13,6 +13,22 @@ func (c App) Index() revel.Result {
 }
 
 func (c App) Hello(myName string) revel.Result {
+	// Explanation of Flash
+	// https://revel.github.io/manual/sessionflash.html#flash
+
+	c.Validation.Required(myName).Message("Name is required!")
+	c.Validation.MinSize(myName, 3).Message("Name is not long enough!")
+
+	if c.Validation.HasErrors() {
+		// Sets the flash parameter `error` which will be sent by a flash cookie
+		c.Flash.Error("Something went wrong!")
+		// Keep the validation error from above by setting a flash cookie
+		c.Validation.Keep()
+		// Copies all given parameters (URL, Form, Multipart) to the flash cookie
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+
 	return c.Render(myName)
 }
 
@@ -23,6 +39,7 @@ func (c App) Robin(first string, third string) revel.Result {
 	if first == "" {
 		first = "No first passed"
 	}
+
 	// This is making up a new parameter called second
 	second := "second"
 	// The names of the parameters here end up as named
