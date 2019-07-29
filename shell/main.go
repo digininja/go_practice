02d824df825480f09988cf8b98038873f824e3f5
Robin <robin@digi.ninja>
2019-07-29 10:10:29 +0100

    running shell commands and parsing the output

---
+++ shell/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	var (
+		cmdOut []byte
+		err    error
+	)
+	cmdName := "git"
+	cmdArgs := []string{"log", "--pretty=full", "--name-only", "--all"}
+
+	if cmdOut, err = exec.Command(cmdName, cmdArgs...).Output(); err != nil {
+		log.Fatal(fmt.Sprintf("There was an error running git command: %s", err))
+	}
+	outputStr := string(cmdOut)
+	// fmt.Println("Output from command: %s", outputStr)
+
+	scanner := bufio.NewScanner(strings.NewReader(outputStr))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "commit") {
+			fmt.Printf("Commit ID: %s\n", line)
+		}
+		//fmt.Println(scanner.Text())
+	}
+}
