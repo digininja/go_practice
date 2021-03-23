3d7fbb80e34832d8588c5d0c6670dd52da8125b2
Robin <robin@digi.ninja>
2021-03-23 20:58:44 +0000

    playing with UUIDs

---
+++ uuid/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+import "github.com/google/uuid"
+
+func main() {
+	fmt.Printf("UUID: %s\n", uuid.New())
+	fmt.Printf("UUID: %s\n", uuid.New())
+	fmt.Printf("UUID: %s\n", uuid.New())
+	fmt.Printf("UUID: %s\n", uuid.New())
+}
