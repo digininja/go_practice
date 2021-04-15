d9d4fce6779fdffefe6b38c18722948501cd03fa
Robin Wood <robin@digi.ninja>
2021-04-15 10:45:18 +0100

    ordering, limiting and wheres

--- gorm/main.go
+++ gorm/main.go
@@ -11,7 +11,63 @@ import (
 func main() {
 	models.ConnectDatabase()
 
-	controllers.CreateURL("https://digi.ninja")
+	create := false
+
+	if create {
+
+		order := models.Order{Name: "and another"}
+		models.DB.Create(&order)
+
+		item1 := models.Item{OrderID: order.ID, Name: "curry", Price: 2233}
+		item2 := models.Item{OrderID: order.ID, Name: "pizza", Price: 90}
+		item3 := models.Item{OrderID: order.ID, Name: "frogs", Price: 180}
+
+		models.DB.Create(&item1)
+		models.DB.Create(&item2)
+		models.DB.Create(&item3)
+
+		controllers.CreateURL("https://digi.ninja")
+	}
+
+	log.Printf("Load an order by ID and get its items")
+	var order models.Order
+	models.DB.First(&order, 2)
+	var retrievedItems []models.Item
+	models.DB.Where("order_id = ?", order.ID).Find(&retrievedItems)
+
+	for _, item := range retrievedItems {
+		log.Printf("Item: %s - Price: %d\n", item.Name, item.Price)
+	}
+
+	log.Printf("Done")
+
+	log.Printf("Loading all orders ordered by creation date")
+	var orders []models.Order
+	models.DB.Order("created_at").Find(&orders)
+
+	for _, order := range orders {
+		var retrievedItems []models.Item
+		models.DB.Where("order_id = ?", order.ID).Find(&retrievedItems)
+		log.Printf("Order name: %s", order.Name)
+		for _, item := range retrievedItems {
+			log.Printf("\tItem: %s - Price: %d\n", item.Name, item.Price)
+		}
+	}
+	log.Printf("Done")
+
+	log.Printf("Loading two orders reverse ordered by creation date")
+
+	models.DB.Limit(2).Order("created_at desc").Find(&orders)
+
+	for _, order := range orders {
+		var retrievedItems []models.Item
+		models.DB.Where("order_id = ?", order.ID).Find(&retrievedItems)
+		log.Printf("Order name: %s", order.Name)
+		for _, item := range retrievedItems {
+			log.Printf("\tItem: %s - Price: %d\n", item.Name, item.Price)
+		}
+	}
+	log.Printf("Done")
 
 	urls := controllers.GetURLs()
 
